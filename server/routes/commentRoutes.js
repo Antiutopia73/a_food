6a8263b2b4b4d35d7ad5d4ab7badb1cc73208f33
Maksim Pokutsa <106033231+Antiutopia73@users.noreply.github.com>
2024-12-06 02:34:39 +0200

    Update commentRoutes.js

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -3,8 +3,8 @@ import { addComment, getComments } from '../controller/comment.js';
 
 const router = express.Router();
 
-router.post('/create', addComment); // Добавить комментарий
-router.get('/receive', getComments); // Получить все комментарии
+router.post('/create', addComment);
+router.get('/receive', getComments);
 
 
 export default router;
